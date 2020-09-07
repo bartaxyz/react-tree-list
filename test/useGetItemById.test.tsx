579a3aa2e6fc9c48e658d3275b89ce21b79fcc9f
Ondřej Bárta <email@ondrejbarta.xyz>
2020-09-07 17:23:50 +0200

    Add tests for useGetItemById

--- test/useGetItemById.test.tsx
+++ test/useGetItemById.test.tsx
@@ -20,4 +20,83 @@ describe("useGetItemById", () => {
 
     wrapper.render();
   });
+
+  it("should fail to find an id in data", () => {
+    const UseUniqueIdComponent = () => {
+      const getItemById = useGetItemById<ReactTreeListItemType>([
+        {
+          id: "A1",
+          label: "A1",
+        },
+        {
+          id: "A2",
+          label: "A2",
+          children: [
+            {
+              id: "B2",
+              label: "B2",
+              children: [
+                {
+                  id: "C2",
+                  label: "C2",
+                },
+              ],
+            },
+          ],
+        },
+      ]);
+
+      const item = getItemById("");
+
+      expect(item).eq(undefined);
+
+      return null;
+    };
+
+    const wrapper = shallow(<UseUniqueIdComponent />);
+
+    wrapper.render();
+  });
+
+  it('should find a nested item with id of "C2" in nested data', () => {
+    const UseUniqueIdComponent = () => {
+      const getItemById = useGetItemById<ReactTreeListItemType>([
+        {
+          id: "A1",
+          label: "A1",
+        },
+        {
+          id: "A2",
+          label: "A2",
+          children: [
+            {
+              id: "B2",
+              label: "B2",
+              children: [
+                {
+                  id: "C2",
+                  label: "C2",
+                },
+              ],
+            },
+          ],
+        },
+      ]);
+
+      const item = getItemById("C2");
+
+      if (item) {
+        expect(item.id).eq("C2");
+        expect(item.label).eq("C2");
+      } else {
+        expect(item).to.not.eq(undefined);
+      }
+
+      return null;
+    };
+
+    const wrapper = shallow(<UseUniqueIdComponent />);
+
+    wrapper.render();
+  });
 });
