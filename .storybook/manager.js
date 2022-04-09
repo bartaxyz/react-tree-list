5509682bc825a7c87fc0a5ef2bd3ddec2047583e
Ondřej Bárta <email@ondrejbarta.xyz>
2022-04-09 18:53:12 +0200

    Update manager.js

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,38 +1,4 @@
 import { addons } from "@storybook/addons";
-import { create } from "@storybook/theming/create";
-
-const Theme = create({
-  base: "light",
-  brandTitle: "@bartaxyz/react-tree-list",
-
-  colorPrimary: "white",
-  colorSecondary: "#222",
-
-  // UI
-  appBg: "rgba(0, 0, 0, 0.025)",
-  appContentBg: "white",
-  appBorderColor: "rgba(0, 0, 0, 0.1)",
-  appBorderRadius: 4,
-
-  // Typography
-  fontBase: '"Open Sans", sans-serif',
-  fontCode: "monospace",
-
-  // Text colors
-  textColor: "#222",
-  textInverseColor: "rgba(255,255,255,0.9)",
-
-  // Toolbar default and active colors
-  barTextColor: "silver",
-  barSelectedColor: "#222",
-  barBg: "white",
-
-  // Form colors
-  inputBg: "white",
-  inputBorder: "silver",
-  inputTextColor: "#222",
-  inputBorderRadius: 4,
-});
 
 addons.setConfig({
   isFullscreen: false,
@@ -42,7 +8,6 @@ addons.setConfig({
   sidebarAnimations: true,
   enableShortcuts: true,
   isToolshown: true,
-  theme: Theme,
   selectedPanel: undefined,
   initialActive: "sidebar",
   showRoots: false,
